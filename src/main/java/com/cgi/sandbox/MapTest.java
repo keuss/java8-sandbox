549d8f74c12a72ecc01de351e29561e242d3cb6a
JOCELIN <feret.jocelin@cgi.com>
2018-12-12 10:32:11 +0100

    Add test Map java 8

---
+++ src/main/java/com/cgi/sandbox/MapTest.java
@@ -0,0 +1,30 @@
+package com.cgi.sandbox;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapTest {
+
+    private static void printerMap(Object key, Object val) {
+        System.out.println("Autre traitement ...");
+        System.out.println(key + "=>" +val);
+    }
+
+    public static void main(String[] args) {
+        Map<Integer, String> stringMap = new HashMap<>();
+
+        for (int i = 0; i < 10; i++) {
+            stringMap.putIfAbsent(i, "val " + i);
+        }
+
+        //(key, val) -> System.out.println(key + "=>" +val);
+        /*stringMap.forEach((key, val) -> {
+                System.out.println("Autre traitement ...");
+                System.out.println(key + "=>" + val);
+            }
+        );*/
+        stringMap.forEach(MapTest::printerMap);
+
+
+    }
+}
